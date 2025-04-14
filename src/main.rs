8ff0b718dc521b9b4c39a70449bf47d2c7fc1e5b
2tefan <me@2tefan.eu>
2025-04-14 17:50:27 +0200

    Replace cronjob crate w/ with self-rolled tokio thread

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,23 @@
 #[macro_use]
 extern crate rocket;
 
-
-mod gitlab;
-mod github;
 mod database;
 mod git_platform;
+mod github;
+mod gitlab;
 
-use cronjob::CronJob;
+use std::time::Duration;
+
 use dotenv::dotenv;
 use git_platform::{GitEvents, GitPlatform};
 use github::Github;
 use gitlab::Gitlab;
-use log::error;
+use log::info;
 use rocket::http::{ContentType, Status};
-use tokio::join;
-use serde::Serialize;
 use rocket::serde::json::Json;
+use serde::Serialize;
+use tokio::join;
+use tokio::time::sleep;
 
 #[derive(Serialize)]
 struct HealthResponse {
@@ -26,7 +27,6 @@ struct HealthResponse {
 async fn fetch_data_from_git_providers() {
     let mut github = Github::init_from_env_vars();
     let mut gitlab = Gitlab::init_from_env_vars();
-
 
     let (_github_result, _gitlab_result) = join!(
         async {
@@ -38,12 +38,6 @@ async fn fetch_data_from_git_providers() {
     );
 }
 
-
-fn fetch_git_providers_cron(name: &str) {
-    println!("{}: It's time!", name);
-    //fetch_data_from_git_providers().await;
-}
-
 #[get("/health")]
 fn health() -> Json<HealthResponse> {
     Json(HealthResponse { status: "ok" })
@@ -51,9 +45,7 @@ fn health() -> Json<HealthResponse> {
 
 #[get("/git-events")]
 async fn get_git_events() -> Json<Vec<GitEvents>> {
-    Json(
-        Gitlab::get_all_git_events().await
-    )
+    Json(Gitlab::get_all_git_events().await)
 }
 
 #[get("/force-sync")]
@@ -61,41 +53,46 @@ async fn force_sync() -> (Status, (ContentType, String)) {
     let dev_mode = std::env::var("POLLUX_ENABLE_DEV_MODE");
     if dev_mode.is_ok() && dev_mode.unwrap().to_ascii_lowercase() == "true" {
         fetch_data_from_git_providers().await;
-        (
-            Status::Ok,
-            (
-                ContentType::Text,
-                "fetching done".to_string(),
-            ),
-        )
-    } else { 
+        (Status::Ok, (ContentType::Text, "fetching done".to_string()))
+    } else {
         (
             Status::Forbidden,
-            (
-                ContentType::Text,
-                "Not allowed in prod!".to_string(),
-            ),
+            (ContentType::Text, "Not allowed in prod!".to_string()),
         )
     }
 }
 
-#[launch]
-fn rocket() -> _ {
+async fn run_cron_job() {
+    loop {
+        sleep(Duration::new(1, 0)).await;
+        info!("Crontime ✨");
+
+        // Run the actual fetching
+        //fetch_data_from_git_providers().await;
+    }
+}
+
+#[rocket::main]
+async fn main() -> Result<(), rocket::Error> {
     dotenv().ok();
     env_logger::init();
 
-    // Setup cronjobs
-    let mut cron = CronJob::new("Test Cron", fetch_git_providers_cron);
-    // Set seconds.
-    cron.seconds("0");
-    // Start the cronjob.
-    CronJob::start_job_threaded(cron);
-
+    // Init git providers
     Gitlab::get_or_init();
     Github::get_or_init();
+
+    // Prepare cronjob
+    tokio::spawn(async {
+        run_cron_job().await
+    });
 
     rocket::build()
         .mount("/", routes![health])
         .mount("/api/v1", routes![force_sync, get_git_events])
+        .launch()
+        .await
+        .unwrap();
+
+    Ok(())
 }
 
